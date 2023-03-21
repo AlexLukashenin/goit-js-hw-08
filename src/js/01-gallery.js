6020897856b59c8b18023e80b2bb0b0d14c530e0
Alex Lukashenin <Alukashenin@gmail.com>
2023-03-21 16:00:12 +0200

    did 3 tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+// пошук галереї
+const gallery = document.querySelector('.gallery');
+const CardMarkup = createImgCardMarkup(galleryItems);
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+gallery.insertAdjacentHTML('beforeend', CardMarkup);
+
+// передача параметрів в функцію
+console.log(createImgCardMarkup(galleryItems));
+
+function createImgCardMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" title = "${description}" alt="${description}" />
+        </a>`;
+    })
+    .join('');
+}
+// додавання затримки для опису, зображення
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,51 @@
+import throttle from 'lodash.throttle';
+//змінна для зберігання ключа 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData ={}
+// Пошук елементів на сторінці
+
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea');
+const email = document.querySelector('input[name="email"]');
+
+// Додаємо слухача події input до форми
+form.addEventListener(
+  'input',
+  throttle(e => {
+    // Об'єкт з полями email і message, у яких зберігаються поточні значення полів форми
+    const formData = { email: email.value, message: textarea.value };
+
+    // Записування у локальне сховище об'єкта з полями.
+    // JSON.stringify - конвертування JS-значень у формат строки JSON.
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }, 500) 
+);
+// Додаємо слухача події submit до форми
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  console.log({email: email.value, message: textarea.value });
+  // очищення поля форми
+  form.reset();
+  // очищення сховища
+  localStorage.removeItem(STORAGE_KEY);
+});
+
+// Метод load для абстрагування повторюваного коду та перевірки помилок парса
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key); // ключ елемента сховища
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+// Присвоєння ключа до сховища
+const storageData = load(STORAGE_KEY);
+if (storageData) {
+email.value = storageData.email;
+  textarea.value = storageData.message;
+} 
 
